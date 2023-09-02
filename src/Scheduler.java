adb660bfcc03f54bd7cbe742896791880d995a33
Jocelyn409 <jocelynlahaise@gmail.com>
2023-09-02 00:45:26 -0400

    updates

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -32,14 +32,30 @@ public class Scheduler {
         }
     }
 
-    private boolean awakenProcesses() {
-        // wherever we wake up the process, make sure that the above is <= a new millis() ! ! !
-        for(int arrayIndex = 0; arrayIndex <= 2; arrayIndex++) {
-            if(processListsArray[arrayIndex].indexOf(runningProcess) > 0) {
-                return true;
+    private void awakenProcesses() {
+        for(int i = 0; i < sleepingProcesses.size(); i++) {
+            if(sleepingProcesses.get(i).getSleepUntil() >= clock.millis()) {
+                addProcess(sleepingProcesses.remove(i));
             }
         }
-        return false;
+    }
+
+    private void addProcess(KernelandProcess process) {
+        switch(process.getLevel()) {
+            case RealTime -> realTimeProcesses.add(process);
+            case Interactive -> interactiveProcesses.add(process);
+            case Background -> backgroundProcesses.add(process);
+        }
+    }
+
+    private void removeRunningProcess() {
+        for(int arrayIndex = 0; arrayIndex <= 2; arrayIndex++) {
+            int runningProcessIndex = processListsArray[arrayIndex].indexOf(runningProcess);
+            if(runningProcessIndex > 0) {
+                processListsArray[arrayIndex].remove(runningProcessIndex);
+                return;
+            }
+        }
     }
 
     public void sleep(int milliseconds) {
@@ -69,11 +85,7 @@ public class Scheduler {
         KernelandProcess newProcess = new KernelandProcess(up, PID++, level);
 
         // Add process to correct list.
-        switch(newProcess.getLevel()) {
-            case RealTime -> realTimeProcesses.add(newProcess);
-            case Interactive -> interactiveProcesses.add(newProcess);
-            case Background -> backgroundProcesses.add(newProcess);
-        }
+        addProcess(newProcess);
         if(runningProcess == null) {
             switchProcess();
         }
@@ -82,21 +94,21 @@ public class Scheduler {
 
     // Stop running process if there is one; add it to the end of the LL
     // if it hasn't finished, then run first process in LL.
-    public void switchProcess() {
+    private void switchProcess() {
         if(runningProcess != null) {
             // If there is a running process, stop it.
             runningProcess.stop();
-            int index = processLinkedList.indexOf(runningProcess); // Get index of the stopped process.
-
-            awakenProcesses();
-
-            if(!(processLinkedList.get(index).isDone())) {
-                // If the process did not finish, add it to end of the LL.
-                processLinkedList.add(processLinkedList.remove(index));
+            removeRunningProcess();
+            if(!(runningProcess.isDone())) {
+                // If the process did not finish, add it to end of the correct LL.
+                addProcess(runningProcess);
             }
             runningProcess = null; // Make runningProcess null as it was stopped.
         }
-        processLinkedList.get(0).run(); // Run first process in LL.
-        runningProcess = processLinkedList.get(0);
+        awakenProcesses();
+
+        // we need random # stuff
+        //processLinkedList.get(0).run(); // Run first process in LL.
+        //runningProcess = processLinkedList.get(0);
     }
 }
