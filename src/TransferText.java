985e5da16dd812059f2498717b10b1359615be38
Jocelyn409 <jocelynlahaise@gmail.com>
2023-10-16 01:16:40 -0400

    stuff

--- src/TransferText.java
+++ src/TransferText.java
@@ -1,11 +1,22 @@
 public class TransferText extends UserlandProcess {
     @Override
     public void run() {
+        // Testing files.
         int indexFrom = OS.Open("file TransferFrom.txt");
         OS.Seek(indexFrom, 0);
-        byte[] bufferFrom = OS.Read(indexFrom, 100);
-        int index0 = OS.Open("file TransferTo.txt");
-        OS.Seek(index0, 0);
-        OS.Write(index0, bufferFrom);
+        byte[] buffer = OS.Read(indexFrom, 1000);
+        int indexTo = OS.Open("file TransferTo.txt");
+        OS.Seek(indexTo, 0);
+        OS.Write(indexTo, buffer);
+        OS.Close(indexFrom);
+
+        // Testing random.
+        int indexRandom = OS.Open("random 100");
+        OS.Seek(indexRandom, 0);
+        byte[] bufferRandom = OS.Read(indexRandom, 10);
+        OS.Seek(indexTo, 0);
+        OS.Write(indexTo, bufferRandom);
+        OS.Close(indexRandom);
+        OS.Close(indexTo);
     }
 }
